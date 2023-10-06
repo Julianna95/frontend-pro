8dac50a3c53c6348b70c97b3732fd3778447d22c
Julianna95 <usika62643@gmail.com>
2023-10-06 22:56:23 +0300

    fixed repo

---
+++ homework2/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let firstNum;
+let secondNum;
+let resultSum;
+let resultDiff;
+let resultMult;
+let resultDiv;
+
+firstNum = Number(prompt('Please enter a number'));
+secondNum = Number(prompt('Please enter another number'));
+resultSum = firstNum + secondNum;
+resultDiff = firstNum - secondNum;
+resultMult = firstNum * secondNum;
+resultDiv = firstNum / secondNum;
+alert('Calculations are finished! \nSum: ' + firstNum + ' + ' + secondNum + ' = ' + resultSum + '\nDiff: ' + firstNum + ' - ' + secondNum + ' = ' + resultDiff + '\nMult: ' + firstNum + ' * ' + secondNum + ' = ' + resultMult + '\nDiv: '+ firstNum + ' / ' + secondNum + ' = ' + resultDiv);
---
+++ homework3/index.js
@@ -0,0 +1,10 @@
+'use strict'
+ 
+let userHours;
+let secInHours;
+let resultInSec;
+    
+userHours = prompt("Please enter a number of hours");
+secInHours = 3600;
+resultInSec = userHours * secInHours;
+alert(resultInSec + ' seconds');
