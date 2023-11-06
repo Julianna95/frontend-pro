18b1f87903d8ea2d49ce5f142dfb8959d6042e59
Julianna95 <usika62643@gmail.com>
2023-11-06 17:39:35 +0200

    added homework 12 'CreateHash'

--- homework12/index.js
+++ homework12/index.js
@@ -1,2 +1,14 @@
 'use strict'
 
+function createHash(key, secret) {
+  return key + secret;
+}
+
+const key = prompt('Enter some key');
+const secret = 'aa22bb33';
+const hash = createHash(key, secret);
+console.log(hash); 
+const hash2 = createHash(key, secret);
+console.log(hash2); 
+const hash3 = createHash(key, secret);
+console.log(hash3); 
