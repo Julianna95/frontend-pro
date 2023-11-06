674f3a5a1ca4c664a131a3e850101194e7fcb54f
Julianna95 <usika62643@gmail.com>
2023-11-06 16:42:59 +0200

    added homework 10 and 11

--- homework11/index.js
+++ homework11/index.js
@@ -1,2 +1,12 @@
 'use strict'
 
+let userNumber = Number(prompt('Enter a number'));
+function calcFactorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * calcFactorial(n - 1);
+  }
+}
+
+alert(`Factorial of ${userNumber} is ${calcFactorial(userNumber)}`); 
--- homework12/index.js
+++ homework12/index.js
@@ -1,12 +1,2 @@
 'use strict'
 
-let userNumber = Number(prompt('Enter a number'));
-function calcFactorial(n) {
-  if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    return n * calcFactorial(n - 1);
-  }
-}
-
-alert(`Factorial of ${userNumber} is ${calcFactorial(userNumber)}`); 
